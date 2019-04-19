04f54676a8559ed74253944c55741e3037c05344
Adam Bronte <adam.bronte@coupa.com>
2019-04-18 21:49:34 -0700

    0.0.3

--- setup.py
+++ setup.py
@@ -5,19 +5,19 @@ from distutils.core import setup
 with open('README.rst') as fp:
     readme = fp.read()
 
+with open('requirements.txt') as fp:
+    requires = fp.read().split('\n')
+
 setup(
     name='PysparkGateway',
-    version='0.0.2',
+    version='0.0.3',
     packages=[
         'pyspark_gateway',
         'pyspark_gateway.patch_files'],
     license='Apache 2.0',
     description='Connect Pyspark to remote clusters',
     long_description=readme,
-    install_requires=[
-        'requests>=2.20.0',
-        'Flask~=1.0.2'
-        ],
+    install_requires=requires,
     extras_require={
         'dev': [
             'pandas',
