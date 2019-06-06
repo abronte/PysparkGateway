d44772c956bc65eace6d4b80f82d2923812049ab
Adam Bronte <adam.bronte@coupa.com>
2019-06-05 19:03:21 -0700

    bump to verison 0.0.12

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as fp:
 
 setup(
     name='PysparkGateway',
-    version='0.0.11',
+    version='0.0.12',
     packages=[
         'pyspark_gateway',
         'pyspark_gateway.patch_files'],
